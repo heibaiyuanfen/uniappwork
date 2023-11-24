a56bf90a1056223520d003c3a441628630db2443
heibaiyuanfen <heibaiyuanfen@gmail.com>
2023-11-24 16:57:56 +0800

    Update tabbar-4.vue

--- pages/tabbar/tabbar-4/tabbar-4.vue
+++ pages/tabbar/tabbar-4/tabbar-4.vue
@@ -1,17 +1,25 @@
 <template>
 	<view class="uni-page-body">
+
+		
+		
 		<view class="calendar-box"
 		style="width: 60%;
-		height: 40px;">
+		height: 40px;
+		position: absolute;
+		top: 250px;">
 		<tn-calendar  v-model="selectDate" mode="date"></tn-calendar>	
 		</view>
 		<!-- <TnCalendar v-model="selectDate" mode="date" /> -->
 
 		<view style="position: absolute;
 		top: 250px;
-		background-color: white;">
+		background-color: white;
+		width: 40%;
+		left: 60%;
+		">
 			<tn-collapse>
-			  <tn-collapse-item v-for="(item, index) in list" :key="index" :title="item.title" :disabled="item.disabled">
+			  <tn-collapse-item v-for="(item, index) in list1" :key="index" :title="item.title" :disabled="item.disabled">
 			    <view class="collapse-item-content">
 			      {{ item.content }}
 			    </view>
@@ -32,7 +40,7 @@ export default {
 			type:2,
 			result: '',
 			lunarResult:'',
-			 list: [{
+			 list1: [{
 			            title: '关雎',
 			            content: '关关雎鸠，在河之洲。窈窕淑女，君子好逑。参差荇菜，左右流之。窈窕淑女，寤寐求之。求之不得，寤寐思服。悠哉悠哉，辗转反侧。参差荇菜，左右采之。窈窕淑女，琴瑟友之。参差荇菜，左右芼之。窈窕淑女，钟鼓乐之。',
 			            disabled: false
@@ -47,7 +55,8 @@ export default {
 			            content: '秋风起兮白云飞，草木黄落兮雁南归。兰有秀兮菊有芳，怀佳人兮不能忘。泛楼船兮济汾河，横中流兮扬素波。少壮几时兮奈老何！',
 			            disabled: false
 			          }
-			        ]
+			        ],
+
 		};
 	},
 	onLoad() {},
@@ -58,14 +67,7 @@ export default {
 </script>
 
 <style>
-.content {
-	
-	text-align: center;
-	height: 400upx;
-	margin-top: 200upx;
-	display: flex;
-	flex-direction: column;
-}
+
   .collapse-item-content {
     word-wrap: break-word;
   }
